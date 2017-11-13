8f291bdd48e991ff92ee715d5bb4230cc16ff7d4
Yunkun Guo <yunkun.dev@gmail.com>
2017-11-13 10:47:22 -0800

    replace the UI

---
+++ Assets/Assets/Scripts/ButtonsOnClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonsOnClick : MonoBehaviour {
+
+	public Button btn1;
+	public Button btn2;
+	public Button btn3;
+	// Use this for initialization
+	void Start () 
+	{
+		
+		btn1.onClick.AddListener (btnPractice);
+		btn2.onClick.AddListener (btnChanllenge);
+		btn3.onClick.AddListener (btnSettings);
+	}
+
+	void btnPractice()
+	{
+		Debug.Log("button1");
+		BoardManager.isPracticeMode = true;
+		TileController.speedScale = 1f;
+		SceneManager.LoadScene(1);
+	}
+
+	void btnChanllenge()
+	{
+		Debug.Log ("button2");
+		BoardManager.isPracticeMode = false;
+		TileController.speedScale = 1.5f;
+		SceneManager.LoadScene(1);
+	}
+
+	void btnSettings()
+	{
+		//Debug.Log ("button3");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
