6c61cfd0776375416c53fb731521dd573b0df080
Yunkun Guo <yunkun.dev@gmail.com>
2017-11-24 23:57:56 -0800

    Add setting and Final

--- Assets/Assets/Scripts/ButtonsOnClick.cs
+++ Assets/Assets/Scripts/ButtonsOnClick.cs
@@ -9,16 +9,19 @@ public class ButtonsOnClick : MonoBehaviour {
 	public Button btn1;
 	public Button btn2;
 	public Button btn3;
+	public Button btn4;
+
 	// Use this for initialization
 	void Start () 
 	{
 		
-		btn1.onClick.AddListener (btnPractice);
+		btn1.onClick.AddListener (btnNormal);
 		btn2.onClick.AddListener (btnChanllenge);
-		btn3.onClick.AddListener (btnSettings);
+		btn3.onClick.AddListener (btnCrazy);
+		btn4.onClick.AddListener (btnSettings);
 	}
 
-	void btnPractice()
+	void btnNormal()
 	{
 		Debug.Log("button1");
 		BoardManager.isPracticeMode = true;
@@ -34,9 +37,18 @@ public class ButtonsOnClick : MonoBehaviour {
 		SceneManager.LoadScene(1);
 	}
 
+	void btnCrazy()
+	{
+		Debug.Log ("button3");
+		BoardManager.isPracticeMode = false;
+		TileController.speedScale = 1.5f;
+		SceneManager.LoadScene (1);
+	}
+
 	void btnSettings()
 	{
-		//Debug.Log ("button3");
+		Debug.Log ("button4");
+		SceneManager.LoadScene (3);
 	}
 
 
--- Assets/Assets/Scripts/ShowFinalScore.cs
+++ Assets/Assets/Scripts/ShowFinalScore.cs
@@ -8,11 +8,37 @@ public class ShowFinalScore : MonoBehaviour {
 
 	public static int score;
 	public Text mytext;
+	public Button btn1;
+	public Button btn2;
+	public Button btn3;
+
+
 	// Use this for initialization
 	void Start () 
 	{
 
 		mytext.text = " " + score;
-		
+
+		btn1.onClick.AddListener (btnMainMenu);
+		btn2.onClick.AddListener (btnReplace);
+		btn3.onClick.AddListener (btnScores);
 	}
+
+	void btnMainMenu()
+	{
+		Debug.Log ("button1");
+		SceneManager.LoadScene (0);
+	}
+
+	void btnReplace()
+	{
+		Debug.Log ("button2");
+		SceneManager.LoadScene (1);
+	}
+
+	void btnScores()
+	{
+		Debug.Log ("button3");
+	}
+
 }
